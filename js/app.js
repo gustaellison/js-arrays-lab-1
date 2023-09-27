c794c807a3d65787cfe83c415662ddaebe6981d9
Augusta Ellison <gustaellison@gmail.com>
2023-09-27 11:52:42 -0400

    completed exercises 1 and 2

--- js/app.js
+++ js/app.js
@@ -1,2 +1,19 @@
 // log something to make sure the file is being loaded
 console.log('my app.js is being run');
+
+// Exercise 1 has been completed for you...
+
+const foods = [];
+
+console.log('Exercise 1 Result:\n', foods);
+
+/*
+Exercise 2:
+- Add the strings 'pizza' & 'cheeseburger' to the foods array such that 'pizza' comes before 'cheeseburger'.
+*/
+
+// Complete Exercise 2 below...
+foods.push('pizza', 'cheeseburger')
+
+
+console.log('Exercise 2 Result:\n',  foods);
